c982179ca7a920e5c408fbc973bb11a1f75ec53a
Jongtae Lee <jt.jongtae@gmail.com>
2012-08-10 18:59:40 +0900

    test case

---
+++ src/test/java/net/daum/clix/test/CampaignTest.java
@@ -0,0 +1,38 @@
+package net.daum.clix.test;
+
+import net.daum.clix.Campaign;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jtlee
+ * Date: 8/10/12
+ * Time: 4:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(JUnit4ClassRunner.class)
+public class CampaignTest {
+
+    @Test
+    public void testSave(){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Transaction tx = session.beginTransaction();
+
+        Campaign campaign = new Campaign();
+        campaign.setName("campaign");
+        campaign.setBudget(1000L);
+         Long id = (Long) session.save(campaign);
+
+        tx.commit();
+
+    }
+}
